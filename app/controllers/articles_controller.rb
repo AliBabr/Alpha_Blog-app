b4dd50d4e93487c2abaa4ccd45d66cfb38930214
Babar Ali <babar2166@gmail.com>
2018-08-27 08:55:11 -0400

    New, edit, delete of Articles complete action with UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article.id)
     else
       render 'new'
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   def update
     @article=Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice]="Article was successfull updated"
+      flash[:success]="Article was successfully updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -35,6 +35,13 @@ class ArticlesController < ApplicationController
   
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
   end
 
   private
