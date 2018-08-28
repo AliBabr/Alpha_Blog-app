14a16cd45afc28921b158985c76662d3892261f4
Babar Ali <babar2166@gmail.com>
2018-08-28 06:12:51 -0400

    Done with association between articles and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180828071143) do
+ActiveRecord::Schema.define(version: 20180828084529) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
