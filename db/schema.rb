6e961dcfe82e31973d7cbd3c25c2a2c939932165
Babar Ali <babar2166@gmail.com>
2018-08-28 04:11:22 -0400

    create user table and add validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818132407) do
+ActiveRecord::Schema.define(version: 20180828071143) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
