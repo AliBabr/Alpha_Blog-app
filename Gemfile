39b09aa057637c89c70bc4df5ddf7482fea10377
Babar Ali <babar2166@gmail.com>
2018-08-29 10:00:12 -0400

    Complete navbar controls with perfect working

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'bootstrap-sass', '~> 3.3.5'
 
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'will_paginate', '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
+
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
  def update
    @user = User.find(params[:id])
     if @user.save
@@ -27,7 +28,16 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
+  
+  def index
+     @users = User.paginate(page: params[:page], per_page: 5)
+  end
+  
+   def show
+      @user = User.find(params[:id])
+      @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+   end
+  
   private
     def user_params
   params.require(:user).permit(:username, :email, :password)
