1e842c0bdfed17a9b5fe03c63f4f0a7085db7b28
Babar Ali <babar2166@gmail.com>
2018-08-30 03:05:29 -0400

    compplete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  
+  before_action :set_article, only:[:edit, :update, :show, :destroy]
+  before_action :require_user, except:[:index, :show]
+  before_action :require_same_user, only:[:edit, :update, :destroy]
  
   def index
      @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -9,7 +13,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article.id)
@@ -19,11 +23,10 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-   @article=Article.find(params[:id])
+   
   end
   
   def update
-    @article=Article.find(params[:id])
     if @article.update(article_params)
       flash[:success]="Article was successfully updated"
       redirect_to article_path(@article)
@@ -34,7 +37,7 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def destroy
@@ -48,4 +51,16 @@ class ArticlesController < ApplicationController
    def article_params
      params.require(:article).permit(:title, :description)
    end
+  
+  def set_article
+    @article = Article.find(params[:id])
+   end
+  
+  def require_same_user
+    if current_user!=@article.user
+      flash[:danger]="You can edit, update and delete your own article"
+      redirect_to article_path
+    end
+  end
+  
 end
--- app/controllers/concerns/users_controller.rb
+++ app/controllers/concerns/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only:[:edit, :update, :show]
+  before_action :require_same_user, only:[:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -16,12 +19,11 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+   
   end
   
  def update
-   @user = User.find(params[:id])
-    if @user.save
+    if @user.update(user_params)
       flash[:success] = "Your account sucessfully updated"
       redirect_to articles_path
     else
@@ -34,7 +36,7 @@ class UsersController < ApplicationController
   end
   
    def show
-      @user = User.find(params[:id])
+      
       @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
    end
   
@@ -43,4 +45,16 @@ class UsersController < ApplicationController
   params.require(:user).permit(:username, :email, :password)
       
     end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+    def require_same_user
+      if current_user!=@user
+        flash[:danger]="You can update your own account"
+        redirect_to root_path
+      end
+     end
+  
 end
